4e2aa16d8f513fcb8dad08353fbe98773da8ffba
Paul K. Gerke <pkcakeout@gmail.com>
2025-11-17 11:30:20 +0100

    Commit unicmmited file listing code

--- main.py
+++ main.py
@@ -2,14 +2,24 @@ import time
 import json
 import random
 import os
+from pathlib import Path
 
 print("EXECUTING...")
 time.sleep(10)
 print(" -> DONE!")
 
+print("INPUTS:")
+input_dir = Path("/input")
+input_list = []
+for path in input_dir.rglob("*"):
+    if path.is_file():
+        rel_path = path.absolute()
+        input_list.append(str(rel_path))
+
 rnd_list = [random.random() for i in range(10)]
 json_data = {
-    "numbers": rnd_list
+    "numbers": rnd_list,
+    "input_files": input_list
 }
 json_str = json.dumps(json_data, indent=4)
 
